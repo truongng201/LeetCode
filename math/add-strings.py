3352def350b2c39f632e496792d7a814ea9ee733
Truong Nguyen <truongng201@gmail.com>
2025-04-03 16:24:48 +0700

    LC-add-strings [Runtime: 8 ms; Memory: 18.2 MB]

--- math/add-strings.py
+++ math/add-strings.py
@@ -1,26 +1,14 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         i, j = len(num1) - 1, len(num2) - 1
-        add = 0
+        carry = 0
         ans = ""
-        while i >= 0 or j >= 0:
-            if j < 0:
-                ans = str(int(num1[:i+1]) + add) + ans
-                add = 0
-                break
-            if i < 0:
-                ans = str(int(num2[:j+1]) + add) + ans
-                add = 0
-                break
-            x = int(num1[i]) + int(num2[j]) + add
-            if x >= 10:
-                add = 1
-                ans = str(x - 10) + ans
-            else:
-                ans = str(x) + ans
-                add = 0
+        while i >= 0 or j >= 0 or carry > 0:
+            digit1 = int(num1[i]) if i >= 0 else 0
+            digit2 = int(num2[j]) if j >= 0 else 0
+            total = digit1 + digit2 + carry
+            carry = total // 10
+            ans = str(total%10) + ans
             i -= 1
-            j -= 1
-        if add == 1:
-            ans = "1" + ans
+            j -=1
         return ans
