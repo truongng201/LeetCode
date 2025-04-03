b34247beeb6def0b2661dfbf65d7d2bbb9fc23a7
Truong Nguyen <truongng201@gmail.com>
2025-04-03 16:19:42 +0700

    LC-add-strings [Runtime: N/A; Memory: N/A]

---
+++ simulation/add-strings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        i, j = len(num1) - 1, len(num2) - 1
+        add = 0
+        ans = ""
+        while i >= 0 or j >= 0:
+            if j < 0:
+                ans = str(int(num1[:i+1]) + add) + ans
+                break
+            if i < 0:
+                ans = str(int(num2[:j+1]) + add) + ans
+                break
+            x = int(num1[i]) + int(num2[j]) + add
+            if x >= 10:
+                add = 1
+                ans = str(x - 10) + ans
+            else:
+                ans = str(x) + ans
+            i -= 1
+            j -= 1
+        return ans
