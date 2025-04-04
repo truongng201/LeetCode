68b8b8714ece48963070c6158a5c24e87cc0b371
Truong Nguyen <truongng201@gmail.com>
2025-04-05 01:18:37 +0700

    LC-powx-n [Runtime: N/A; Memory: N/A]

---
+++ math/powx-n.py
@@ -0,0 +1,12 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def new_pow(x, n):
+            if n == 1 or n == -1:
+                return x ** n
+            mid = n // 2
+            remain = n % 2
+            a = new_pow(x, mid)
+            a *= a
+            a *= x ** remain
+            return a
+        return round(new_pow(x, n), 4)
