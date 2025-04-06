4c30635528db8dffbde31004fce24f3b8d2917f0
Truong Nguyen <truongng201@gmail.com>
2025-04-06 23:46:42 +0700

    LC-divide-intervals-into-minimum-number-of-groups [Runtime: N/A; Memory: N/A]

--- two-pointers/divide-intervals-into-minimum-number-of-groups.py
+++ two-pointers/divide-intervals-into-minimum-number-of-groups.py
@@ -1,19 +1,14 @@
+import heapq
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
         intervals.sort(key=lambda x:x[0])
-        resources = []
+        pq = []
         ans = 0
         for item in intervals:
-            ans = max(ans, len(resources))
             start, finish = item
-            i = 0
-            con = False
-            while i < len(resources):
-                if resources[i][1] < start:
-                    resources[i] = item
-                    break
-                i += 1
-            else:
-                resources.append(item)
-        ans = max(ans, len(resources))
+            if pq and pq[0][0] < finish: 
+                ans -= 1
+                heapq.heappop(pq)
+            heapq.heappush(pq, (finish, start))
+            ans += 1           
         return ans
