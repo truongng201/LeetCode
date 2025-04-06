6ed47c9af4496a9af373bc4430c61fccbaedef59
Truong Nguyen <truongng201@gmail.com>
2025-04-06 23:55:40 +0700

    LC-merge-intervals [Runtime: N/A; Memory: N/A]

---
+++ array/merge-intervals.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x:x[1])
+        ans = [intervals[0]]
+        for i in range(1, len(intervals)):
+            if ans and intervals[i][0] <= ans[-1][1]:
+                new_interval = [ans[-1][0], intervals[i][1]]
+                ans.pop()
+                ans.append(new_interval)
+                continue
+            ans.append(intervals[i])
+        return ans
