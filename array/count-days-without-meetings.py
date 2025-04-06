f9c942c876d03c7d6e14a4f1c200974087fe2364
Truong Nguyen <truongng201@gmail.com>
2025-04-07 01:12:30 +0700

    LC-count-days-without-meetings [Runtime: 174 ms; Memory: 53 MB]

--- array/count-days-without-meetings.py
+++ array/count-days-without-meetings.py
@@ -3,10 +3,12 @@ class Solution:
         cnt = 0
         meetings.sort(key=lambda x:x[0])
         curr = meetings[0]
+        if curr[0] > 1:
+            cnt += curr[0] - 1
         for i in range(1, len(meetings)):
             if meetings[i][0] > curr[1]:
                 cnt += meetings[i][0] - curr[1] - 1
-            curr = meetings[i]
+            curr = [min(curr[0], meetings[i][0]), max(curr[1], meetings[i][1])]
         if curr[1] < days:
             cnt += days - curr[1]
         return cnt
