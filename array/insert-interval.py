4863a1a5a8c7c66c0500fc14812ad2511d7a58c2
Truong Nguyen <truongng201@gmail.com>
2025-04-07 00:09:55 +0700

    LC-insert-interval [Runtime: N/A; Memory: N/A]

---
+++ array/insert-interval.py
@@ -0,0 +1,19 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        ans = []
+        for idx, item in enumerate(intervals):
+            if newInterval[0] >= item[0] and newInterval[0] <= item[1]:
+                new_item = [item[0], max(item[1], newInterval[1])]
+                print(new_item)
+                ans.append(new_item)
+                idx += 1
+                while ans and intervals[idx][0] <= ans[-1][1]:
+                    new_item = [ans[-1][0], max(ans[-1][1], intervals[idx][1])]
+                    ans.pop()
+                    ans.append(new_item)
+                    idx += 1
+                ans += intervals[idx:]
+                break
+            else:
+                ans.append(item)
+        return ans
