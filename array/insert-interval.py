ef913994f0b91aebe5b408b50c32a0e6f724c6f4
Truong Nguyen <truongng201@gmail.com>
2025-04-07 00:31:52 +0700

    LC-insert-interval [Runtime: N/A; Memory: N/A]

--- array/insert-interval.py
+++ array/insert-interval.py
@@ -1,26 +1,33 @@
 class Solution:
+    
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         if len(intervals) == 0:
             return [newInterval]
+        def isOverlap(item1, item2):
+            start1, end1 = item1
+            start2, end2 = item2
+            if end1 < start2 or end2 < start1:
+                return False
+            return True
         ans = []
-        if intervals[0][0] > newInterval[0]:
-            ans.append([min(intervals[0][0], newInterval[0]), max(intervals[0][1], newInterval[1])])
-            if len(intervals) > 1:
-                ans += intervals[1:]
-            return ans
+        isSet = False
         for idx, item in enumerate(intervals):
-            if newInterval[0] >= item[0] and newInterval[0] <= item[1]:
-                new_item = [item[0], max(item[1], newInterval[1])]
-                ans.append(new_item)
+            if isOverlap(newInterval, item):
+                start, end = newInterval
+                isSet = True
+                ans.append([min(start, item[0]), max(end, item[1])])
                 idx += 1
-                while ans and idx < len(intervals) and intervals[idx][0] <= ans[-1][1]:
-                    new_item = [ans[-1][0], max(ans[-1][1], intervals[idx][1])]
-                    ans.pop()
-                    ans.append(new_item)
-                    idx += 1
-                ans += intervals[idx:]
-                return ans
+                break
             else:
                 ans.append(item)
-        ans.append(newInterval)
+        while idx < len(intervals):
+            if ans and isOverlap(ans[-1], intervals[idx]):
+                new_item = [min(ans[-1][0], intervals[idx][0]), max(ans[-1][1], intervals[idx][1])]
+                ans.pop()
+                ans.append(new_item)
+            else:
+                ans.append(intervals[idx])
+            idx += 1
+        if not isSet:
+            ans.append(newInterval)
         return ans
