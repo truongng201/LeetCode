259e49b815b2971dd671ced75e3ddf4054d6f42e
Truong Nguyen <truongng201@gmail.com>
2025-05-01 22:25:04 +0700

    LC-longest-increasing-subsequence [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/longest-increasing-subsequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        dp = [0]*(n + 1)
+        
+        for i in range(1, n + 1):
+            for j in range(0, i):
+                if nums[i - 1] > nums[j]:
+                    dp[i] = max(dp[i], dp[j + 1])
+            dp[i] += 1
+
+        return dp[n]
