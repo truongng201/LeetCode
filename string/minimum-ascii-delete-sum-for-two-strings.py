92da0ca99a2d7bf1474fe07c1e80ae45052cf77a
Truong Nguyen <truongng201@gmail.com>
2025-05-05 11:23:38 +0700

    LC-minimum-ascii-delete-sum-for-two-strings [Runtime: 383 ms; Memory: 22 MB]

--- string/minimum-ascii-delete-sum-for-two-strings.py
+++ string/minimum-ascii-delete-sum-for-two-strings.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(1, len_1 + 1):
             dp[i][0] = dp[i - 1][0] + ord(s1[i - 1])
         for i in range(1, len_2 + 1):
-            dp[0][i] = dp[i - 1][0] + ord(s2[i - 1])
+            dp[0][i] = dp[0][i - 1] + ord(s2[i - 1])
         
         for i in range(1, len_1 + 1):
             for j in range(1, len_2 + 1):
